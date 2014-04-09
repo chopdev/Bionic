7df76f57eb131bc8ddaa5f413d8d426f6a00cf9b
alex-pichurin <sedova26@mail.ru>
2014-04-09 20:43:46 +0300

    qwerty

--- BionicProject/BionicProject/AModule.cs
+++ BionicProject/BionicProject/AModule.cs
@@ -5,15 +5,67 @@ using System.Text;
 using System.Threading.Tasks;
 
 
+///////////////////////////////////
+using MySql.Data.MySqlClient;
+///////////////////////////////////
+
 
 namespace BionicProject
 {
-    class AModule
+    partial class StoreDB
     {
+        string s_GetUsersOnCourse = "SELECT * FROM `UserCourses` JOIN `User` on UserCourses.UserID=User.UserID WHERE CourseID=@ID";  //Выборка всех юзеров на определенном курсе
+
+        public List<User> GetUsersOnCourse(Course cr)
+        {
+            User user = null;
+            MySqlCommand cmd = database.CreateCommand();
+
+            cmd.CommandText = s_GetUsersOnCourse;
+            cmd.Parameters.AddWithValue("@ID", cr.CourseId);
+
+            try
+            {
+                database.Open();
+
+                MySqlDataReader data = cmd.ExecuteReader();
+                while (data.Read())
+                {
+                    
+                }
+
+            }
+            catch
+            {
+                System.Windows.MessageBox.Show("Не удалось подключиться к БД");
+
+            }
+            finally
+            {
+
+                database.Close();
+            }
+            return user;
+        }
 
     }
 
 
+    public class AdminSelection
+    {
+        Course course;
+
+        int userid;
+        CourseStatus status;
+        string Fname;
+        string Lname;
+        string email;
+
+        public AdminSelection(Course SelectedCourse)
+        {
+            course = SelectedCourse;
+        }
+    }
 
 
 }
