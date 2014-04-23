de9bd3e0b846d686eb102d5e1a0dafadd01e0a13
Sergiy-illichevskyy <sergiy.illichevskyy@gmail.com>
2014-04-23 20:52:48 +0300

    Serg2

---
+++ BionicProject/AModule.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+///////////////////////////////////
+using MySql.Data.MySqlClient;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+///////////////////////////////////
+
+namespace BionicProject
+{
+    partial class StoreDB
+    {
+       
+        string s_GetUsersOnCourse = "SELECT * FROM `UserCourses` JOIN `User` on UserCourses.UserID=User.UserID WHERE CourseID=@ID";  
+        //Выборка всех юзеров на определенном курсе
+        string s_UpUserStatus = "UPDATE `UserCourses` SET `CourseStatus` = @St  WHERE `UserCourses`.`CourseID` =  @c_id AND `UserCourses`.`UserID` = @u_id;";
+        //Поменять роль телу на курсе
+        
+        //Удалить тело с курса
+        string s_deleteOnCourse = "DELETE FROM `UserCourses` WHERE `UserID`=@U_id and `CourseID`=@C_id";
+
+
+        public List<AdminSelection> GetUsersOnCourse(Course cr)
+        {
+            List<AdminSelection> selection = new List<AdminSelection>();
+            MySqlCommand cmd = Connection.CreateCommand();
+
+            cmd.CommandText = s_GetUsersOnCourse;
+            cmd.Parameters.AddWithValue("@ID", cr.CourseId);
+
+            try
+            {
+                Connection.Open();
+
+                MySqlDataReader data = cmd.ExecuteReader();
+                while (data.Read())
+                {
+                    selection.Add(new AdminSelection(Int32.Parse(data["UserID"].ToString()),Int32.Parse(data["CourseStatus"].ToString()),
+                        data["FirstName"].ToString(),data["LastName"].ToString(),data["Email"].ToString()));
+                }
+
+            }
+            catch
+            {
+                System.Windows.MessageBox.Show("Не удалось подключиться к БД");
+
+            }
+            finally
+            {
+
+               Connection.Close();
+            }
+            return selection;
+        }
+
+        public bool UpdateStatusOnCourse(Course cr, int ID, CourseStatus CS)
+        {
+            MySqlCommand cmd = Connection.CreateCommand();
+
+            cmd.CommandText = s_UpUserStatus;
+            cmd.Parameters.AddWithValue("@St", CS);
+            cmd.Parameters.AddWithValue("@c_id", cr.CourseId);
+            cmd.Parameters.AddWithValue("@u_id", ID);
+            bool result = true;
+            try
+            {
+                Connection.Open();
+
+                cmd.ExecuteNonQuery();
+                
+          
+            }
+            catch
+            {
+                System.Windows.MessageBox.Show("Не удалось подключиться к БД");
+                result = false;
+            }
+            finally
+            {
+
+                Connection.Close();
+            }
+            return result;
+        }
+
+        public bool DeleteUserOnCurse(Course cr, int ID)
+        {
+            MySqlCommand cmd = Connection.CreateCommand();
+
+            cmd.CommandText = s_deleteOnCourse;
+            cmd.Parameters.AddWithValue("@C_id", cr.CourseId);
+            cmd.Parameters.AddWithValue("@U_id", ID);
+            bool result = true;
+            try
+            {
+                Connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                System.Windows.MessageBox.Show("Не удалось подключиться к БД");
+                result = false;
+            }
+            finally
+            {
+
+                Connection.Close();
+            }
+            return result;
+        }
+    }
+
+    public class AdminSelection:INotifyPropertyChanged
+    {
+        public string FIO { get { return Lname + " " + Fname; } }
+        int userid;
+        public int UserID { get { return userid; } set{SetField(ref userid, value, "UserID");}  }
+        CourseStatus status;
+        public CourseStatus Status { get { return status; } set { SetField(ref status, value, "Status"); } }
+        string Fname;
+        public string FirstName { get { return Fname; } }
+        string Lname;
+        public string LastName { get { return Lname; } }
+        string email;
+        public string Email { get { return email; } }
+        public AdminSelection(int userid, int status, string Fname, string Lname, string email)
+        {
+          this.userid = userid;
+          this.status = (CourseStatus)status;
+          this.Fname = Fname;
+          this.Lname = Lname;
+          this.email = email;
+        }
+
+        #region
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetField<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion
+
+        public AdminSelection Clone()
+        {
+            return new AdminSelection(this.userid, (int)this.status, this.Fname, this.Lname, this.email);
+        }
+
+    }
+
+    public class AdminSelections : ObservableCollection<AdminSelection>
+    {
+        StoreDB store = new StoreDB();
+
+        public AdminSelections AllusersInCourse;
+
+        public AdminSelections(Course cr)
+        {
+
+            AllusersInCourse = new AdminSelections();
+
+            foreach (var m in store.GetUsersOnCourse(cr))
+            { Add(m); AllusersInCourse.Add(m.Clone()); }
+        }
+
+
+        AdminSelections()
+        {
+
+        }
+
+        public AdminSelection[] CloneCollection()
+        {
+            AdminSelection[] n_select = new AdminSelection[this.Count];
+            int i = 0;
+            foreach (var m in this)
+            {
+                n_select[i++] = m.Clone(); 
+            }
+            return n_select;
+        }
+    }
+
+}
---
+++ BionicProject/TeacherQuestionAddingControl.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BionicProject
+{
+    /// <summary>
+    /// Interaction logic for TeacherQuestionAddingControl.xaml
+    /// </summary>
+    public partial class TeacherQuestionAddingControl : UserControl
+    {
+        private Course currentCourse;
+        public List<Answer> answers;
+        public Question question;
+        
+
+        public TeacherQuestionAddingControl(Course currentCourse)
+        {
+            InitializeComponent();
+            this.currentCourse = currentCourse;
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            answers = null;
+            lvAnswersList.ItemsSource = null;
+            cbDifficulty.SelectedIndex = 0;
+            cbCountOfAnswers.SelectedIndex = -1;
+            tbQuestionsText.Text = "";
+            question = new Question("",QuestionType.Checkbox,1,currentCourse.CourseId);
+            gridMain.DataContext = question;
+            
+        }
+
+        private void cbCountOfAnswers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int countOfAnswers = cbCountOfAnswers.SelectedIndex +1;
+             answers = new List<Answer>();
+            for (int i = 0; i < countOfAnswers; i++)
+            {
+                answers.Add(new Answer("",question.QuestionId,false));
+            }
+            lvAnswersList.ItemsSource = answers;
+            //lvAnswersList
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //проверки
+            if (string.IsNullOrEmpty(tbQuestionsText.Text))
+            {
+                MessageBox.Show("Заполните текст вопроса", "Error", MessageBoxButton.OK,MessageBoxImage.Warning);
+                return;
+            }
+            if (answers==null)
+            {
+                MessageBox.Show("Заполните ответы на вопрос", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            int countOfCorrectAnswers = 0;
+            int countOfAnswers = cbCountOfAnswers.SelectedIndex + 1;
+            for (int i = 0; i < countOfAnswers; i++)
+            {
+                if(answers[i].IsCorrect)
+                    countOfCorrectAnswers++;    
+                if (string.IsNullOrEmpty(answers[i].AnswerText))
+                {
+                    MessageBox.Show("Заполните все ответы", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+            }
+            if (countOfCorrectAnswers == 0)
+            {
+                MessageBox.Show("Хотя бы один ответ должен быть правильным", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            //проверки
+
+            TeacherModule.SaveQuestion(question);
+            countOfCorrectAnswers = 0;
+            for (int i = 0; i < countOfAnswers; i++)
+            {
+                answers[i].QuestionsId = question.QuestionId;
+                if (answers[i].IsCorrect)
+                    countOfCorrectAnswers++;
+                TeacherModule.SaveAnswer(answers[i]);
+            }
+            if (countOfAnswers == 1)
+            {
+                question.QuestionType = QuestionType.Text;
+            }
+            else if(countOfAnswers>1 && countOfCorrectAnswers==1)
+            {
+                question.QuestionType = QuestionType.Radiobutton;
+            }
+            else
+            {
+                question.QuestionType = QuestionType.Checkbox;
+            }
+            TeacherModule.SaveQuestion(question);
+            Refresh();
+        }
+    }
+}
